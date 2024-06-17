8f89d62b9cde9ae7421ae84d9e376b97634c5bd9
Rounakharde <61427269+Rounakharde@users.noreply.github.com>
2024-06-17 09:28:55 +0000

    data is conversion data file add

---
+++ basicjs/conversionOpertion.js
@@ -0,0 +1,26 @@
+let score="Ram";
+
+// console.log(typeof score) // value is null output print  object 
+// console.log(typeof score) // string 
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)//o/p =number
+// console.log(valueInNumber)//NAN bug in js 
+// console.log(valueInNumber)//thne value is null they convert into number value is 0
+// console.log(valueInNumber)//value is undefine thye convet into Nana
+//the value is true the output is 1
+//the value is false the output is 0
+//the vlaue is String type so output is NAN(not an number)
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+//1 => true;
+//0=>false
+//""=> false
+//"hitesh"=>true
+
+let someNum= 33;
+let stringNumber=String(someNum)
+console.log(stringNumber)
+
